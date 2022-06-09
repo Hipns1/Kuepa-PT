ee7899487e7aba44f002c55e41e2c6d3dfac0ab6
Hipns1 <jesudpf21@hotmail.com>
2022-06-08 21:22:14 -0500

    Mejora de componente de estadisticas

---
+++ src/Components/Conexiones.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from "../Styles/Conexiones.module.scss";
+
+const Conexiones = () => {
+    return (
+        <div className={styles.conexiones_container}>
+            <div className={styles.conexiones}>
+            </div>
+
+            <div className={styles.plan}>
+            
+            </div>
+        </div>
+    )
+}
+
+export default Conexiones
