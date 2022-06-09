784058fc7b5063af7da58dd09570e0c27ec44ae6
Hipns1 <jesudpf21@hotmail.com>
2022-06-08 19:25:50 -0500

    filter y estadisticas terminadas

--- src/Components/Stats.js
+++ src/Components/Stats.js
@@ -1,18 +1,54 @@
 import React from 'react';
 import styles from '../Styles/Stats.module.scss';
-import ProgressBar from 'react-customizable-progressbar';
+import { CircularProgressBar } from "@tomik23/react-circular-progress-bar";
 
 const Stats = () => {
 
     const progress = 50;
+    const props = {
+        percent: progress,
+        colorCircle: "#e3e3e3",
+        size: 150,
+        round: true,
+        stroke: 5,
+        linearGradient: ["#f17b97", "#fb908b"],
+    };
 
     return (
         <div className={styles.stat_container}>
             <section className={styles.stat_title}>
                 <h1>Hola, Jorge</h1>
             </section>
-            <section className={styles.stat_content}>
 
+            <section className={styles.stat_stat}>
+                <section className={styles.stat_content}>
+                    <CircularProgressBar {...props} />
+                </section>
+                <section className={styles.stat_lead}>
+                    <div className={styles.stat_text}>
+                        <h1>Leads obtenidos</h1>
+                        <span>50/100</span>
+                    </div>
+                    <div className={styles.stat_text}>
+                        <h1>Cola de llamados</h1>
+                        <span>50/100</span>
+                    </div>
+                    <div className={styles.stat_text}>
+                        <h1>Matriculas finalizadas</h1>
+                        <span>10/20</span>
+                    </div>
+                </section>
+            </section>
+            <section className={styles.stat_meta}>
+                <h1>Meta</h1>
+                <div>
+                    <h1>Leads obtenidos <span>200/400</span></h1>
+                    
+                </div>
+                <div>
+                    <h1>Matriculas realizadas <span>50/100</span></h1>
+                    
+                </div>
             </section>
         </div>
     )
